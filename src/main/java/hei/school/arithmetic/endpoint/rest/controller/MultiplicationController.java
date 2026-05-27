4a64453a6d5d7a5beaabe01a91bc099352f6b4d4
Thiannight <thianntatiana@gmail.com>
2026-05-27 14:03:45 +0300

    feat: add multiplication service and controller

---
+++ src/main/java/hei/school/arithmetic/endpoint/rest/controller/MultiplicationController.java
@@ -0,0 +1,23 @@
+package hei.school.arithmetic.endpoint.rest.controller;
+
+import hei.school.arithmetic.model.OperationRequest;
+import hei.school.arithmetic.model.OperationResult;
+import hei.school.arithmetic.service.MultiplicationService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/arithmetic")
+@AllArgsConstructor
+public class MultiplicationController {
+
+    private final MultiplicationService multiplicationService;
+
+    @PostMapping("/multiplication")
+    public OperationResult multiply(@RequestBody OperationRequest request) {
+        return new OperationResult(multiplicationService.multiply(request.a(), request.b()));
+    }
+}
